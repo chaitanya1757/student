4a408601c28235015eca2de8fae562cc45fda322
Chaitanya <chaitanya@predikkta.com>
2022-02-14 23:07:45 +0530

    - Student component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentEntryComponent } from './student-entry/student-entry.component';
+import { StudentViewComponent } from './student-view/student-view.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentEntryComponent,
+    StudentViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
